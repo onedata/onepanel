77dcc0d055cb0e7a88a007b147cf3630680255b6
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2014-05-29 18:50:39 +0200

    VFS-642 Saving key and cert files on all nodes.

--- src/spanel_modules/user_modules/user_logic.erl
+++ src/spanel_modules/user_modules/user_logic.erl
@@ -13,6 +13,7 @@
 -include("spanel_modules/db.hrl").
 -include("spanel_modules/errors.hrl").
 -include("registered_names.hrl").
+-include("spanel_modules/install.hrl").
 
 %% API
 -export([hash_password/1, authenticate/2, change_password/3, register_in_global_registry/0]).
@@ -59,17 +60,32 @@ change_password(Username, OldPassword, NewPassword) ->
     _ -> {error, ?AUTHENTICATION_ERROR}
   end.
 
+%% register_in_global_registry/0
+%% ====================================================================
+%% @doc Registers provider in global registry.
+%% @end
+-spec register_in_global_registry() -> ok | error.
+%% ====================================================================
 register_in_global_registry() ->
   try
     {ok, Dir} = application:get_env(?APP_NAME, grp_dir),
-    {ok, Password} = application:get_env(?APP_NAME, default_password),
+    {ok, KeyName} = application:get_env(?APP_NAME, grpkey_name),
     {ok, KeyPath} = application:get_env(?APP_NAME, grpkey_file),
     {ok, CsrPath} = application:get_env(?APP_NAME, grpcsr_file),
+    {ok, CertName} = application:get_env(?APP_NAME, grpcert_name),
     {ok, CertPath} = application:get_env(?APP_NAME, grpcert_file),
     {ok, Url} = application:get_env(?APP_NAME, global_registry_url),
+    Path = ?DEFAULT_NODES_INSTALL_PATH ++ ?DEFAULT_WORKER_NAME ++ "/certs",
     file:make_dir(Dir),
-    0 = pkcs10:create_csr(Password, KeyPath, CsrPath),
+    0 = pkcs10:create_csr("", KeyPath, CsrPath),
     {ok, Csr} = file:read_file(CsrPath),
+    {ok, Key} = file:read_file(KeyPath),
+    case install_utils:save_file_on_hosts(Path, KeyName, Key) of
+      ok -> ok;
+      {error, ErrorHosts} ->
+        lager:error("Failed to save private key on following hosts: ~p", [ErrorHosts]),
+        throw(error)
+    end,
     Urls = "[" ++ string:join(lists:map(fun(Host) -> "\"" ++ Host ++ "\"" end, install_utils:get_hosts()), ", ") ++ "]",
     Body = "{\"urls\" : " ++ Urls ++ ", \"csr\" : \"" ++ binary_to_list(Csr) ++ "\", \"redirectionPoint\" : \"https://127.0.0.1:8080\"}",
     {ok, "200", _ResponseHeaders, ResponseBody} = ibrowse:send_req(Url ++ "/provider", [{content_type, "application/json"}], post, Body),
@@ -79,9 +95,11 @@ register_in_global_registry() ->
       undefined -> throw(error);
       _ ->
         ok = file:write_file(CertPath, Cert),
-        case install_utils:save_cert_on_hosts(Cert) of
+        case install_utils:save_file_on_hosts(Path, CertName, Cert) of
           ok -> ok;
-          {error, ErrorHosts} -> lager:error("Failed to save certificate on following hosts: ~p", [ErrorHosts])
+          {error, FailedHosts} ->
+            lager:error("Failed to save certificate on following hosts: ~p", [FailedHosts]),
+            throw(error)
         end
     end,
     ok
