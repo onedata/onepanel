4f32a742311c918db867793c636fb1389f2d7f8c
Tomasz Lichon <lichon@student.agh.edu.pl>
2015-05-07 20:08:02 +0300

    VFS-1100 Typos fixed

--- src_oneprovider/onepanel_modules/installer/installer_worker.erl
+++ src_oneprovider/onepanel_modules/installer/installer_worker.erl
@@ -222,7 +222,7 @@ local_start(MainCCM, OptCCMs, Dbs, StoragePaths) ->
             ]
         ),
 
-        ServiceStart = "/etc/init.d/ " ++ atom_to_list(?SOFTWARE_NAME) ++ " start > /dev/null",
+        ServiceStart = "/etc/init.d/" ++ atom_to_list(?SOFTWARE_NAME) ++ " start > /dev/null",
         SetUlimitsCmd = installer_utils:get_system_limits_cmd(Host),
         "" = os:cmd("bash -c \"" ++ SetUlimitsCmd ++ " ; " ++ ServiceStart ++ "\""),
 
@@ -245,7 +245,7 @@ local_stop() ->
     try
         ?debug("Stopping worker node on host: ~p", [Host]),
 
-        ServiceStop = "service " ++ atom_to_list(?SOFTWARE_NAME) ++ " stop > /dev/null",
+        ServiceStop = "/etc/init.d/" ++ atom_to_list(?SOFTWARE_NAME) ++ " stop > /dev/null",
         "" = os:cmd(ServiceStop),
 
         {ok, Host}
