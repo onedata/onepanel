729e3d6b0302b86988e9d548f06c0f4034abae84
Jakub Liput <jakub.liput@gmail.com>
2017-07-14 09:10:22 +0200

    VF-3345 Updating GUI ref to 17.06.0-beta6

--- gui-config.sh
+++ gui-config.sh
@@ -21,6 +21,6 @@
 TARGET_DIR='_build/default/lib/gui_static'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-3283'
+PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:17.06.0-beta6'
 # Image which will be used if primary image is not resolved.
-SECONDARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-3283'
+SECONDARY_IMAGE='onedata/onepanel-gui:17.06.0-beta6'
