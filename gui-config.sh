7c2a0a66217f503bd6c5d2ef01e9de7452797136
Jakub Liput <jakub.liput@gmail.com>
2017-08-07 13:59:54 +0200

    VFS-3298 Updating GUI to VFS-3298

--- gui-config.sh
+++ gui-config.sh
@@ -21,6 +21,6 @@
 TARGET_DIR='_build/default/lib/gui_static'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-3360'
+PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-3298'
 # Image which will be used if primary image is not resolved.
-SECONDARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-3360'
+SECONDARY_IMAGE='onedata/onepanel-gui:VFS-3298'
