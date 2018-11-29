b031dd772d26a16d6722659e6d6b61a20682ec71
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-11-29 12:30:01 +0100

    VFS-5107 Use graph sync to retrieve supported spaces:

--- src/services/modules/service_oneprovider.erl
+++ src/services/modules/service_oneprovider.erl
@@ -570,7 +570,8 @@ revoke_space_support(#{id := SpaceId}) ->
 %%--------------------------------------------------------------------
 -spec get_spaces(Ctx :: service:ctx()) -> list().
 get_spaces(_Ctx) ->
-    {ok, SpaceIds} = oz_providers:get_spaces(provider),
+    [Node | _] = service_op_worker:get_nodes(),
+    {ok, SpaceIds} = rpc:call(Node, provider_logic, get_spaces, []),
     [{ids, SpaceIds}].
 
 
