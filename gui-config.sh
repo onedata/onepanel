9f05086ec40a5c205e85b3a567a82d1cffcd6c2a
Jakub Liput <jakub.liput@gmail.com>
2019-03-05 17:28:58 +0100

    Updating GUI to: VFS-5301-additional-webdav-fields-18.02.1 * VFS-5301 Add additional fields to WebDAV storage

--- gui-config.sh
+++ gui-config.sh
@@ -21,6 +21,6 @@
 TARGET_DIR='_build/default/lib/gui_static'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-5187-spaces-support-fixes'
+PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:VFS-5301-additional-webdav-fields-18.02.1'
 # Image which will be used if primary image is not resolved.
-SECONDARY_IMAGE='onedata/onepanel-gui:VFS-5187-spaces-support-fixes'
+SECONDARY_IMAGE='onedata/onepanel-gui:VFS-5301-additional-webdav-fields-18.02.1'
