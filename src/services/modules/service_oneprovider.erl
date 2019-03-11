b0144e1dd87b6ac88c66bc1d20359f4d83d7073c
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-11 13:04:13 +0100

    VFS-4751 Fix is_registered check

--- src/services/modules/service_oneprovider.erl
+++ src/services/modules/service_oneprovider.erl
@@ -419,10 +419,11 @@ unregister() ->
 is_registered(#{node := Node}) ->
     try rpc:call(Node, oneprovider, is_registered, []) of
         true -> true;
-        falses -> false;
+        false -> false;
         _ ->
             case service:get(name()) of
-                {ok, #service{ctx = C}} -> maps:get(registered, C, false)
+                {ok, #service{ctx = #{registered := Registered}}} -> Registered;
+                _ -> false
             end
     catch _:_ ->
         false
