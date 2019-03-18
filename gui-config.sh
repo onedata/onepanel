c06c3c4e3d67bfb31da50f119f45076419581407
Jakub Liput <jakub.liput@gmail.com>
2019-03-18 17:59:22 +0100

    VFS-4751 GUI update

--- gui-config.sh
+++ gui-config.sh
@@ -22,6 +22,6 @@ TARGET_DIR='_build/default/lib'
 ARCHIVE_NAME='gui_static.tar.gz'
 # Image which will be used by default to get the static files. If it cannot
 # be resolved, the script will fall back to secondary.
-PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:ID-ff9bd45a72' #@fixme proper commit
+PRIMARY_IMAGE='docker.onedata.org/onepanel-gui:ID-2926016332' #@fixme proper commit
 # Image which will be used if primary image is not resolved.
 SECONDARY_IMAGE='no-secondary-image'
