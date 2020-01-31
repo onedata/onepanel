bae2207a21d502152d74874c5429f00bf85a54a3
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-01-31 14:11:13 +0100

    VFS-6075 Define test cases verifying certificate migration

--- test_distributed/service_onepanel_test_SUITE.erl
+++ test_distributed/service_onepanel_test_SUITE.erl
@@ -6,6 +6,9 @@
 %%% @end
 %%%--------------------------------------------------------------------
 %%% @doc This module contains integration tests of onepanel service.
+%%%
+%%% This test suites recreates test nodes for each test case
+%%% to ensure fresh state for clusterization tests.
 %%% @end
 %%%--------------------------------------------------------------------
 -module(service_onepanel_test_SUITE).
@@ -33,6 +36,7 @@
     extend_should_add_node_by_ip/1,
     extend_should_return_hostname_of_new_node/1,
     extend_should_work_in_deployed_cluster/1,
+    extend_should_copy_certificates_to_new_node/1,
     leave_should_remove_node/1
 ]).
 
@@ -46,6 +50,7 @@ all() ->
         extend_should_add_node_by_hostname,
         extend_should_add_node_by_ip,
         extend_should_return_hostname_of_new_node,
+        extend_should_copy_certificates_to_new_node,
         leave_should_remove_node
     ]).
 
@@ -186,7 +191,7 @@ leave_should_remove_node(Config) ->
 extend_should_add_node_by_hostname(Config) ->
     [Node1, Node2 | _] = ?config(onepanel_nodes, Config),
     Nodes = [Node1, Node2],
-    [Host1 | Host2] = Hosts = lists:sort(hosts:from_nodes(Nodes)),
+    [Host1, Host2] = Hosts = lists:sort(hosts:from_nodes(Nodes)),
 
     Ctx = #{cookie => ?COOKIE},
     ?assertEqual(ok, rpc:call(Node1, service, apply,
@@ -236,6 +241,32 @@ extend_should_return_hostname_of_new_node(Config) ->
     ?assertEqual(Hosts, lists:sort(rpc:call(Node1, service_onepanel, get_hosts, []))),
     ?assertEqual(Hosts, lists:sort(rpc:call(Node2, service_onepanel, get_hosts, []))).
 
+
+extend_should_copy_certificates_to_new_node(Config) ->
+    [Node1, Node2 | _] = ?config(onepanel_nodes, Config),
+    Nodes = [Node1, Node2],
+    [Host1, Host2] = Hosts = lists:sort(hosts:from_nodes(Nodes)),
+    Host2Bin = list_to_binary(Host2),
+
+    % prepare
+    onepanel_test_utils:service_action(Node1,
+        ?SERVICE_PANEL, deploy, #{hosts => [Host1]}),
+    onepanel_test_utils:service_action(Node1,
+        ?SERVICE_LE, deploy, #{letsencrypt_plugin => ?SERVICE_OPW}),
+    onepanel_test_utils:service_action(Node1,
+        % mock will produce cert and key files
+        ?SERVICE_LE, update, #{letsencrypt_enabled => true}),
+    % sanity check
+    verify_letsencrypt_files(Node1),
+
+    % add node
+    onepanel_test_utils:service_action(Node1,
+        ?SERVICE_PANEL, extend_cluster, #{hostname => Host2Bin}
+    ),
+
+    verify_letsencrypt_files(Node2).
+
+
 %%%===================================================================
 %%% SetUp and TearDown functions
 %%%===================================================================
@@ -275,9 +306,9 @@ init_per_testcase(extend_should_work_in_deployed_cluster, Config) ->
     Cluster1Hosts = hosts:from_nodes(Cluster1),
     onepanel_test_utils:service_action(Node1,
         ?SERVICE_PANEL, deploy, #{hosts => Cluster1Hosts}),
-    test_utils:mock_new(Cluster1, [service_oneprovider]),
-    test_utils:mock_expect(Cluster1, service_oneprovider, get_hosts,
-        fun() -> Cluster1Hosts end),
+    ?assertEqual(ok, test_utils:mock_new(Cluster1, [service_oneprovider])),
+    ?assertEqual(ok, test_utils:mock_expect(Cluster1, service_oneprovider, get_hosts,
+        fun() -> Cluster1Hosts end)),
 
     [{cluster1, Cluster1} | Config2];
 
@@ -295,9 +326,56 @@ init_per_testcase(leave_should_not_remove_used_host, Config) ->
     end, [couchbase, cluster_manager, op_worker]),
     NewConfig;
 
+init_per_testcase(extend_should_copy_certificates_to_new_node, Config) ->
+    NewConfig = init_per_testcase(default, Config),
+    [Node1 | _] = Nodes = ?config(onepanel_nodes, NewConfig),
+    [Host1 | _] = Hosts = hosts:from_nodes(Nodes),
+
+    [Node1, Node2 | _] = ?config(onepanel_nodes, NewConfig),
+    Cluster1 = [Node1, Node2],
+%%    test_utils:mock_new(Cluster1, [service_op_worker]),
+%%    ct:pal("Sleeping", []),
+%%    timer:sleep(timer:seconds(5)),
+    ct:pal("Mocking on nodes ~p", [Cluster1]),
+    ?assertEqual(ok, test_utils:mock_new(Nodes,
+        [service_op_worker, letsencrypt_api], [passthrough])),
+    test_utils:mock_expect(Nodes, service_op_worker, supports_letsencrypt_challenge,
+        fun(_) -> true end),
+    test_utils:mock_expect(Nodes, service_op_worker, get_domain,
+        fun() -> list_to_binary(Host1) end),
+    test_utils:mock_expect(Nodes, service_op_worker, reload_webcert,
+        fun(_) -> ok end),
+    test_utils:mock_expect(Nodes, letsencrypt_api, run_certification_flow,
+        fun(_, _) ->
+            ?assertEqual(ok, file:write_file(onepanel_env:get(web_cert_file),
+                <<"web_cert_file">>)),
+            ?assertEqual(ok, file:write_file(onepanel_env:get(web_key_file),
+                <<"web_key_file">>)),
+            ?assertEqual(ok, file:write_file(onepanel_env:get(web_cert_chain_file),
+                <<"web_cert_chain_file">>)),
+
+            KeysDir = filename:join(
+                onepanel_env:get(letsencrypt_keys_dir),
+                production
+            ),
+            ?assertEqual(ok, filelib:ensure_dir([KeysDir, "/"])),
+            ?assertEqual(ok, file:write_file(KeysDir ++ "/letsencrypt_public_key.pem",
+                <<"letsencrypt_public_key">>)),
+            ?assertEqual(ok, file:write_file(KeysDir ++ "/letsencrypt_private_key.pem",
+                <<"letsencrypt_private_key">>))
+        end),
+    NewConfig;
+
 init_per_testcase(_Case, Config) ->
-    onepanel_test_utils:ensure_started(
-        test_node_starter:prepare_test_environment(Config, ?MODULE)).
+    Config2 = onepanel_test_utils:ensure_started(
+        test_node_starter:prepare_test_environment(Config, ?MODULE)
+    ),
+    Nodes = ?config(onepanel_nodes, Config2),
+    lists:foreach(fun(Node) ->
+        ?assertMatch({ok, _}, rpc:call(Node, mock_manager, start, []))
+    end, Nodes),
+    [{onepanel_nodes, lists:sort(Nodes)}
+        | proplists:delete(onepanel_nodes, Config2)].
 
 
 end_per_testcase(_Case, Config) ->
@@ -306,3 +384,23 @@ end_per_testcase(_Case, Config) ->
 
 end_per_suite(_Config) ->
     ok.
+
+
+%% @private
+-spec verify_letsencrypt_files(node()) -> ok.
+verify_letsencrypt_files(Node) ->
+    {ok, WebCertFile} = test_utils:get_env(Node, ?APP_NAME, web_cert_file),
+    {ok, WebKeyFile}  = test_utils:get_env(Node, ?APP_NAME, web_key_file),
+    {ok, WebCertChainFile}   = test_utils:get_env(Node, ?APP_NAME, web_cert_chain_file),
+    {ok, LEDir} = test_utils:get_env(Node, ?APP_NAME, letsencrypt_keys_dir),
+    LEPrivateKey = filename:join([LEDir, production, "letsencrypt_private_key.pem"]),
+    LEPublicKey = filename:join([LEDir, production, "letsencrypt_public_key.pem"]),
+
+    ?assertEqual({ok, <<"web_cert_file">>}, rpc:call(Node, file, read_file, [WebCertFile])),
+    ?assertEqual({ok, <<"web_key_file">>}, rpc:call(Node, file, read_file, [WebKeyFile])),
+    ?assertEqual({ok, <<"web_cert_chain_file">>}, rpc:call(Node, file, read_file, [WebCertChainFile])),
+
+    ?assertEqual({ok, <<"letsencrypt_private_key">>},
+        rpc:call(Node, file, read_file, [LEPrivateKey])),
+    ?assertEqual({ok, <<"letsencrypt_public_key">>},
+        rpc:call(Node, file, read_file, [LEPublicKey])).
