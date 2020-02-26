d7a7d60cb2ff10ab3b7856c356140144a75cc400
Wojciech Geisler <wojciech.geisler@gmail.com>
2020-02-26 15:20:13 +0100

    VFS-6076 Add op_worker:add_nodes test

--- test_distributed/services_test_SUITE.erl
+++ test_distributed/services_test_SUITE.erl
@@ -35,6 +35,7 @@
     service_op_worker_get_storages_test/1,
     service_oneprovider_unregister_register_test/1,
     service_op_worker_update_storage_test/1,
+    service_op_worker_add_node_test/1,
     services_status_test/1,
     services_stop_start_test/1
 ]).
@@ -65,6 +66,7 @@ all() ->
         service_oneprovider_unregister_register_test,
         service_op_worker_add_storage_test,
         service_op_worker_update_storage_test,
+        service_op_worker_add_node_test,
         services_status_test
         %% TODO VFS-4056
         %% services_stop_start_test
@@ -359,15 +361,33 @@ service_op_worker_update_storage_test(Config) ->
     end, ExistingStorages).
 
 
+service_op_worker_add_node_test(Config) ->
+    AllHosts = ?config(all_hosts, Config),
+    OldHosts = ?config(op_worker_hosts, Config),
+    NewHost = hd(AllHosts -- OldHosts),
+    OldNode = nodes:service_to_node(?SERVICE_PANEL, hd(OldHosts)),
+    NewNode = nodes:service_to_node(?SERVICE_PANEL, NewHost),
+    OldOpNode = nodes:service_to_node(?SERVICE_OPW, OldNode),
+
+    TokenFilePath = onepanel_env:get_remote(OldNode,
+        op_worker_root_token_path, ?APP_NAME),
+    {ok, CurrentFileContents} = rpc:call(OldNode, file, read_file, [TokenFilePath]),
+
+    onepanel_test_utils:service_action(OldNode, ?SERVICE_OPW, add_nodes,
+        #{new_hosts => [NewHost]}),
+
+    ?assertEqual(true, rpc:call(NewNode, service, is_healthy, [?SERVICE_OPW])),
+    ?assertEqual({ok, CurrentFileContents},
+        rpc:call(NewNode, file, read_file, [TokenFilePath])),
+    {ok, OpwNodesList} = ?assertMatch({ok, _},
+        image_test_utils:proxy_rpc(OldNode,
+            OldOpNode, node_manager, get_cluster_nodes, [])),
+    ?assertEqual(length(OldHosts) + 1, length(OpwNodesList)).
+
+
 services_status_test(Config) ->
     lists:foreach(fun({NodesType, MainService, Services}) ->
-        Nodes = case Services of
-            ?SERVICE_OPW ->
-                % op-worker is not deployed everywhere
-                nodes:service_to_nodes(?SERVICE_PANEL, ?config(op_worker_hosts, Config));
-            _ ->
-                ?config(NodesType, Config)
-        end,
+        Nodes = ?config(NodesType, Config),
         lists:foreach(fun(Service) ->
             SModule = service:get_module(Service),
             lists:foreach(fun(Node) ->
