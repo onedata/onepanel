91be01e42f5382fa3da8616ba0a9d3b95a781266
duleba.pio <duleba.pio@gmail.com>
2021-03-18 12:51:27 +0100

    VFS-7026 add io_test after storage added

--- test_distributed/suites/api/oneprovider/api_oneprovider_posix_test_SUITE.erl
+++ test_distributed/suites/api/oneprovider/api_oneprovider_posix_test_SUITE.erl
@@ -31,6 +31,7 @@
     add_correct_storage_skip_detection/1,
     add_bad_storage_perform_detection/1,
     add_bad_storage_skip_detection/1
+
 ]).
 
 
@@ -42,7 +43,9 @@ all() -> [
     add_bad_storage_skip_detection
 ].
 
-
+-define(SUPPORT_SIZE, 10000000).
+-define(ATTEMPTS, 15).
+-define(SAMPLE_FILE_CONTENT, <<"abcdef">>).
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -60,7 +63,11 @@ add_bad_storage_perform_detection(Config) ->
 add_bad_storage_skip_detection(Config) ->
     add_storage_test_base(Config, posix, bad_args, true).
 
+
+%% @private
+-spec add_storage_test_base(test_config:config(), atom, correct_args | bad_args, true | false) -> boolean().
 add_storage_test_base(Config, StorageType, ArgsCorrectness, SkipStorageDetection) ->
+    MemRef = api_test_memory:init(),
     ProviderId = oct_background:get_provider_id(krakow),
     ProviderPanelNodes = oct_background:get_provider_panels(krakow),
 
@@ -71,7 +78,7 @@ add_storage_test_base(Config, StorageType, ArgsCorrectness, SkipStorageDetection
             target_nodes = ProviderPanelNodes,
             client_spec = #client_spec{
                 correct = [
-                    root,
+
                     {member, [?CLUSTER_UPDATE]}
                 ],
                 unauthorized = [
@@ -81,15 +88,32 @@ add_storage_test_base(Config, StorageType, ArgsCorrectness, SkipStorageDetection
                 ],
                 forbidden = [peer]
             },
-            data_spec = build_add_storage_data_spec(StorageType, ArgsCorrectness),
-            prepare_args_fun = build_add_storage_prepare_args_fun(SkipStorageDetection),
-            validate_result_fun = build_add_posix_storage_validate_result_fun(ArgsCorrectness, SkipStorageDetection)
-            }
+            setup_fun = build_add_posix_storage_setup_fun(MemRef),
+            data_spec = build_add_storage_data_spec(MemRef, StorageType, ArgsCorrectness),
+            prepare_args_fun = build_add_storage_prepare_args_fun(MemRef, SkipStorageDetection),
+            validate_result_fun = build_add_posix_storage_validate_result_fun(ArgsCorrectness, SkipStorageDetection),
+            verify_fun = build_add_posix_storage_verify_fun(MemRef, ArgsCorrectness, SkipStorageDetection)
+
+        }
     ])).
 
-build_add_storage_data_spec(posix, correct_args) ->
+
+%% @private
+-spec build_add_posix_storage_setup_fun(api_test_memory:env_ref()) ->
+    api_test_runner:setup_fun().
+build_add_posix_storage_setup_fun(MemRef) ->
+    fun() ->
+        ExistingStorages = opw_test_rpc:get_storages(krakow),
+        api_test_memory:set(MemRef, existing_storages, ExistingStorages)
+    end.
+
+
+%% @private
+-spec build_add_storage_data_spec(api_test_memory:env_ref(), atom, correct_args | bad_args) ->
+    api_test_runner:data_spec().
+build_add_storage_data_spec(MemRef, posix, correct_args) ->
     StorageName = str_utils:rand_hex(10),
-    node_cache:put(storage_name, StorageName),
+    api_test_memory:set(MemRef, storage_name, StorageName),
     #data_spec{
         required_with_custom_error = [
             {<<"type">>, ?ERROR_MISSING_REQUIRED_VALUE(iolist_to_binary([StorageName, <<".type">>]))},
@@ -102,9 +126,9 @@ build_add_storage_data_spec(posix, correct_args) ->
             <<"mountPoint">> => [<<"/volumes/persistence/storage">>]
         }
     };
-build_add_storage_data_spec(posix, bad_args) ->
+build_add_storage_data_spec(MemRef, posix, bad_args) ->
     StorageName = str_utils:rand_hex(10),
-    node_cache:put(storage_name, StorageName),
+    api_test_memory:set(MemRef, storage_name, StorageName),
     #data_spec{
         required_with_custom_error = [
             {<<"type">>, ?ERROR_MISSING_REQUIRED_VALUE(iolist_to_binary([StorageName, <<".type">>]))},
@@ -118,9 +142,12 @@ build_add_storage_data_spec(posix, bad_args) ->
     }.
 
 
-build_add_storage_prepare_args_fun(SkipStorageDetection) ->
+%% @private
+-spec build_add_storage_prepare_args_fun(api_test_memory:env_ref(), true| false) ->
+    api_test_runner:prepare_args_fun().
+build_add_storage_prepare_args_fun(MemRef, SkipStorageDetection) ->
     fun(#api_test_ctx{data = Data}) ->
-        StorageName = node_cache:get(storage_name),
+        StorageName = api_test_memory:get(MemRef, storage_name),
         RequestBody = #{
             StorageName => maps:put(<<"skipStorageDetection">>, SkipStorageDetection, Data)
         },
@@ -132,15 +159,86 @@ build_add_storage_prepare_args_fun(SkipStorageDetection) ->
     end.
 
 
-build_add_posix_storage_validate_result_fun(correct_args, true) ->
-    api_test_validate:http_204_no_content();
-build_add_posix_storage_validate_result_fun(correct_args, false) ->
+%% @private
+-spec build_add_posix_storage_validate_result_fun(bad_args | correct_args, true| false) ->
+    api_test_runner:validate_result_fun().
+build_add_posix_storage_validate_result_fun(correct_args, _) ->
     api_test_validate:http_204_no_content();
 build_add_posix_storage_validate_result_fun(bad_args, true) ->
     api_test_validate:http_204_no_content();
 build_add_posix_storage_validate_result_fun(bad_args, false) ->
     HostNames = api_test_utils:to_hostnames(oct_background:get_provider_nodes(krakow)),
     api_test_validate:http_400_bad_request(?ERROR_ON_NODES(?ERROR_STORAGE_TEST_FAILED(write), HostNames)).
+
+
+%% @private
+-spec build_add_posix_storage_verify_fun(api_test_memory:env_ref(), bad_args | correct_args, true| false) ->
+    api_test_runner:verify_fun().
+build_add_posix_storage_verify_fun(MemRef, bad_args, false) ->
+    fun
+        (_, _) ->
+            StoragesBeforeTest = api_test_memory:get(MemRef, existing_storages),
+            StoragesAfterTest = opw_test_rpc:get_storages(krakow),
+            ?assertEqual(StoragesBeforeTest, StoragesAfterTest),
+            true
+    end;
+build_add_posix_storage_verify_fun(MemRef, ArgsCorrectness, _SkipStorageDetection) ->
+    fun
+        (expected_success, _) ->
+            StoragesBeforeTest = api_test_memory:get(MemRef, existing_storages),
+            StoragesAfterTest = opw_test_rpc:get_storages(krakow),
+            [NewStorageId] = ?assertMatch([_], lists:subtract(StoragesAfterTest, StoragesBeforeTest)),
+            case ArgsCorrectness of
+                correct_args -> ?assertEqual(ok, perform_io_test_on_storage(NewStorageId));
+                bad_args -> ?assertEqual(error, perform_io_test_on_storage(NewStorageId))
+            end,
+            true;
+        (expected_failure, _) ->
+            StoragesBeforeTest = api_test_memory:get(MemRef, existing_storages),
+            StoragesAfterTest = opw_test_rpc:get_storages(krakow),
+            ?assertEqual(StoragesBeforeTest, StoragesAfterTest),
+            true
+    end.
+
+
+%%%===================================================================
+%%% Helpers
+%%%===================================================================
+
+
+%% @private
+-spec perform_io_test_on_storage(binary()) -> ok | error.
+perform_io_test_on_storage(StorageId) ->
+    {UserSession, SpaceName} = create_and_support_space_with_storage(StorageId),
+    FilePath = <<"/", SpaceName/binary, "/test_file">>,
+    case opw_test_rpc:lfm_create(krakow, UserSession, FilePath) of
+        {error, _} -> error;
+        {ok, Guid} ->
+            OpenHandle = opw_test_rpc:lfm_open(krakow, UserSession, {guid, Guid}, rdwr),
+
+            {WriteHandle, Size} = opw_test_rpc:lfm_write(krakow, OpenHandle, 0, ?SAMPLE_FILE_CONTENT),
+            ?assertEqual(length(binary_to_list(?SAMPLE_FILE_CONTENT)), Size),
+
+            {_ReadHandle, Data} = opw_test_rpc:lfm_read(krakow, WriteHandle, 0, Size),
+            ?assertEqual(?SAMPLE_FILE_CONTENT, Data)
+    end.
+
+
+%% @private
+-spec create_and_support_space_with_storage(binary()) -> {binary(), binary()}.
+create_and_support_space_with_storage(StorageId) ->
+    SpaceName = str_utils:rand_hex(10),
+    UserId = oct_background:get_user_id(joe),
+    SpaceId = ozw_test_rpc:create_space(UserId, SpaceName),
+    Token = ozw_test_rpc:create_space_support_token(UserId, SpaceId),
+    {ok, SerializedToken} = tokens:serialize(Token),
+    opw_test_rpc:support_space(krakow, StorageId, SerializedToken, ?SUPPORT_SIZE),
+    UserSessId = oct_background:get_user_session_id(UserId, krakow),
+
+    ?assertMatch(SpaceId, opw_test_rpc:get_user_space_by_name(krakow, UserSessId, UserId, SpaceName)),
+    ?assertEqual(true, lists:member(SpaceId, opw_test_rpc:get_spaces(krakow)), ?ATTEMPTS),
+
+    {UserSessId, SpaceName}.
 
 
 %%%===================================================================
@@ -150,7 +248,8 @@ build_add_posix_storage_validate_result_fun(bad_args, false) ->
 
 init_per_suite(Config) ->
     oct_background:init_per_suite(Config, #onenv_test_config{
-        onenv_scenario = "storages_api_tests"
+        onenv_scenario = "storages_api_tests",
+        envs = [{op_worker, op_worker, [{fuse_session_grace_period_seconds, 24 * 60 * 60}]}]
     }).
 
 end_per_suite(_Config) ->
