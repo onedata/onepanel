29246c50f411b933afba938151b3b827ec4ad57f
duleba.pio <duleba.pio@gmail.com>
2021-11-23 23:46:37 +0100

    VFS-8172 use remove unused functions from test_rpc_api

--- src/utils/test_rpc_api.erl
+++ src/utils/test_rpc_api.erl
@@ -25,21 +25,7 @@
     unset_emergency_passphrase/0,
     verify_emergency_passphrase/1,
     list_onepanel_deployment/0,
-    is_onepanel_initialized/0,
-    service_apply_sync/3,
-    service_apply/4,
-    service_get_ctx/1,
-    get_result_from_service_action/2,
-    get_result_from_service_action/3,
-    list_ceph_pool/0,
-    ensure_loopdevice/2,
-    lvm_create_physical_volume/1,
-    lvm_create_logical_volume/2,
-    lvm_create_volume_group/2,
-    ceph_mon_list_quorum/0,
-    ceph_mgr_list_running/0,
-    ceph_cli_osd_df/0,
-    add_storage/1
+    is_onepanel_initialized/0
 ]).
 
 
@@ -99,75 +85,3 @@ is_onepanel_initialized() ->
         ok -> true;
         _ -> false
     end.
-
-
--spec service_apply_sync(service:name(), service:action(), service:step_ctx()) ->
-    service_executor:results() | errors:error().
-service_apply_sync(Service, Action, Ctx) ->
-    service:apply_sync(Service, Action, Ctx).
-
-
--spec service_apply(service:name(), service:action(), service:step_ctx(), pid()) ->
-    service_executor:results() | errors:error().
-service_apply(Service, Action, Ctx, Self) ->
-    service:apply(Service, Action, Ctx, Self).
-
-
-
--spec service_get_ctx(service:name()) -> term().
-service_get_ctx(Service) ->
-    service:get_ctx(Service).
-
-
--spec get_result_from_service_action(service:name(), service:action()) -> term() | errors:error().
-get_result_from_service_action(Service, Action) ->
-    middleware_utils:result_from_service_action(Service, Action).
-
-
--spec get_result_from_service_action(service:name(), service:action(), service:step_ctx()) -> term() | errors:error().
-get_result_from_service_action(Service, Action, Ctx) ->
-    middleware_utils:result_from_service_action(Service, Action, Ctx).
-
-
--spec list_ceph_pool() -> [binary()].
-list_ceph_pool() ->
-    ceph_pool:list().
-
-
--spec ensure_loopdevice(binary(), integer()) -> binary().
-ensure_loopdevice(Path, Size) ->
-    loopdevice:ensure_loopdevice(Path, Size).
-
-
--spec lvm_create_physical_volume(binary()) -> ok.
-lvm_create_physical_volume(Device) ->
-    lvm:create_physical_volume(Device).
-
--spec lvm_create_logical_volume(binary(), binary()) -> ok.
-lvm_create_logical_volume(VolumeName, GroupName) ->
-    lvm:create_logical_volume(VolumeName, GroupName).
-
-
--spec lvm_create_volume_group(binary(), [binary()]) -> ok.
-lvm_create_volume_group(GroupName, Devices) ->
-    lvm:create_volume_group(GroupName, Devices).
-
-
--spec ceph_mon_list_quorum()-> [binary()].
-ceph_mon_list_quorum()->
-    service_ceph_mon:list_quorum().
-
-
--spec ceph_mgr_list_running()-> [binary()].
-ceph_mgr_list_running()->
-    service_ceph_mgr:list_running().
-
-
--spec ceph_cli_osd_df() -> map().
-ceph_cli_osd_df() ->
-    ceph_cli:osd_df().
-
-
--spec add_storage(map()) -> {binary(), {ok, binary()}} | errors:error().
-add_storage(Storage) ->
-    service_op_worker:add_storage(Storage).
