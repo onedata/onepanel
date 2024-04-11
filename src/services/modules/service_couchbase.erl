561984af047ba33c355a7029634f66c5fc289935
Darin Nikolow <darnik22@gmail.com>
2024-04-11 14:00:43 +0200

    VFS-11267 Fixing couchbase cluster init

--- src/services/modules/service_couchbase.erl
+++ src/services/modules/service_couchbase.erl
@@ -1,4 +1,4 @@
-z%%%--------------------------------------------------------------------
+%%%--------------------------------------------------------------------
 %%% @author Krzysztof Trzepla
 %%% @copyright (C) 2016 ACK CYFRONET AGH
 %%% This software is released under the MIT license
